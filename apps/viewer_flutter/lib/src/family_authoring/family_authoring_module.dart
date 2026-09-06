06fca289d444b03f00a8599820a9dfc461d825d7
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-06 09:32:27 +0500

    chore(family): export parameter resolver

--- apps/viewer_flutter/lib/src/family_authoring/family_authoring_module.dart
+++ apps/viewer_flutter/lib/src/family_authoring/family_authoring_module.dart
@@ -9,6 +9,7 @@ export 'family_file_store.dart';
 export 'family_geometry.dart';
 export 'family_library_dialog.dart';
 export 'family_mesh_importer.dart';
+export 'family_parameter_resolver.dart';
 export 'family_plan_symbol.dart';
 export 'family_sketch_canvas.dart';
 export 'family_validation.dart';
