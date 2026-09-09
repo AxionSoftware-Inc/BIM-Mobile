67a6d8120afd31a144094decbabfadcc14645c75
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 20:56:42 +0500

    refactor(elements): move slab module to domain

---
+++ apps/viewer_flutter/lib/src/features/elements/domain/modules/slab_element_module.dart
@@ -0,0 +1,14 @@
+import '../bim_element_module.dart';
+
+final class SlabElementModule extends BimElementModule {
+  const SlabElementModule()
+      : super(
+          kindKey: 'slab',
+          displayName: 'Slab',
+          typeFamily: BimElementTypeFamily.slab,
+          inspectorAdapterKey: BimElementInspectorKeys.surface,
+          aliases: const <String>{'slab'},
+          isLevelHosted: true,
+          levelLockedByDefault: true,
+        );
+}
