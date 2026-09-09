7ee244b264795d42e2f7bbc1f6e2dbbf9b938716
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 20:54:51 +0500

    refactor(elements): move door module to domain

---
+++ apps/viewer_flutter/lib/src/features/elements/domain/modules/door_element_module.dart
@@ -0,0 +1,16 @@
+import '../bim_element_module.dart';
+
+final class DoorElementModule extends BimElementModule {
+  const DoorElementModule()
+      : super(
+          kindKey: 'door',
+          displayName: 'Door',
+          typeFamily: BimElementTypeFamily.door,
+          inspectorAdapterKey: BimElementInspectorKeys.opening,
+          aliases: const <String>{'door', 'opening'},
+          isLevelHosted: true,
+          isPlanCore: true,
+          isOpening: true,
+          levelLockedByDefault: true,
+        );
+}
