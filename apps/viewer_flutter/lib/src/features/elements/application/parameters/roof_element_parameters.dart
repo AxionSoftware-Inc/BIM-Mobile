2c79c58fb6f46d11e7b12d281ac94dea7d53eab2
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 21:25:11 +0500

    refactor(elements): move roof parameters to application adapter

---
+++ apps/viewer_flutter/lib/src/features/elements/application/parameters/roof_element_parameters.dart
@@ -0,0 +1,35 @@
+import '../../../../render_scene_models.dart';
+import '../element_parameter_values.dart';
+
+/// Typed compatibility read-model for roof instances.
+final class RoofElementParameters {
+  const RoofElementParameters({
+    required this.assemblyId,
+    required this.levelId,
+    required this.roofType,
+    required this.slopeDegrees,
+    required this.overhangMeters,
+  });
+
+  factory RoofElementParameters.fromObject(RenderSceneObject object) {
+    final roofTypeName = elementParameterText(object, 'roof_type');
+    final roofType = roofTypeName == 'SimpleGable'
+        ? 1
+        : roofTypeName == 'AutoFootprint'
+            ? 2
+            : 0;
+    return RoofElementParameters(
+      assemblyId: elementParameterInt(object, 'assembly_id') ?? 0,
+      levelId: object.levelId,
+      roofType: roofType,
+      slopeDegrees: elementParameterDouble(object, 'slope_degrees'),
+      overhangMeters: elementParameterDouble(object, 'overhang_meters'),
+    );
+  }
+
+  final int assemblyId;
+  final int? levelId;
+  final int roofType;
+  final double? slopeDegrees;
+  final double? overhangMeters;
+}
