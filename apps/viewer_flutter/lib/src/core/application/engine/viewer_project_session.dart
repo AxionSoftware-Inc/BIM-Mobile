58d92a054c1f44065fc18cb852214a2b33e6d88c
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-11 09:04:52 +0500

    refactor(core): canonicalize project session imports

--- apps/viewer_flutter/lib/src/core/application/engine/viewer_project_session.dart
+++ apps/viewer_flutter/lib/src/core/application/engine/viewer_project_session.dart
@@ -1,4 +1,4 @@
-import '../../../render_scene_models.dart';
+import '../render_scene/render_scene_models.dart';
 import 'viewer_authoring_gateway.dart';
 import 'viewer_element_creation_gateway.dart';
 import 'viewer_project_gateway.dart';
